5c1f7305737816bee9917839b9c986c2a70fb0ef
sjouljian <sjouljian@koein.com>
2020-08-27 14:11:02 +0300

    Update before_plugin_install.js

--- bin/before_plugin_install.js
+++ bin/before_plugin_install.js
@@ -2,8 +2,8 @@
 
 module.exports = function (context) {
   //var req = context.requireCordovaModule,
-  req = require,
-      path = req ('path'),
+  //req = require,
+    var path = require('path'),
       pathParse = require ('./lib/path-parse');
 
   path.parse = path.parse || pathParse;
